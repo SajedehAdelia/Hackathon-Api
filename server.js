720671327451192044ab5f59bb6ac9a2e5080368
CactusNormal7 <jules.besson7@gmail.com>
2023-10-04 11:07:38 +0200

    d

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
-const songController = require('./controllers/songController');
 const app = express();
 const dotenv = require('dotenv'); // Import dotenv here
 
-dotenv.config(); // Load environment variables
+dotenv.config();
 
-const mongoString = process.env.DATABASE_URL; // Use the DATABASE_URL from environment variables
+const mongoString = "mongodb+srv://adeliafathipour:LDSXERJHylcEwlmp@cluster0.oevbmuu.mongodb.net/;"
 mongoose.connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true }); // Add options
 const database = mongoose.connection;
 
@@ -19,9 +18,6 @@ database.once('open', () => {
     console.log('Database connected');
 });
 
-app.use(express.json());
-app.use('/api', songController);
-app.use('/api', routes);
 
 app.get('/status', (request, response) => {
     const status = {
